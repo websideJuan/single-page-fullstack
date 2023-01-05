b8ff8ace7496ed747b84811d52fc36ea75bd0b09
juanHerrera <portador1051@gmail.com>
2023-01-05 00:05:23 -0300

    App single-page beta.1.1.1 05-01-2023

--- app.js
+++ app.js
@@ -12,26 +12,94 @@ const wrapper_oferta = document.getElementById('wrapper__oferta');
 const template__card__oferta = document.getElementById('template__card__oferta').content;
 
 
+const pintandoCart = document.querySelector('.cart')
 
-window.addEventListener("DOMContentLoaded", async e => {
+document.addEventListener('click', e => {
+
     e.stopPropagation()
 
-    const resultData = await fetchDataProduct()
+    
+ 
+    procecandoDatosdelCarito(e)
 
-    dataProductDestacado(resultData.productosDestacados)
-    dataProductOfertas(resultData.productosOferta)
+    showPopUp(e)
+
 })
 
-const dataProductOfertas = (datas) => {
+
+const procecandoDatosdelCarito = (e) => {
+    
+    let cartShopItems = []
+    
+    if(e.target.classList.contains('btn--product')) {
+
+        const refElementDOM = e.target.parentElement
+
+        const objFrom = {
+            title: refElementDOM.querySelector('.card__title').textContent,
+            price: refElementDOM.querySelector('.card__price > span').textContent,
+            count: 1,
+            id: refElementDOM.querySelector('.btn--product').dataset.id
+        }
+
+
+
+        if(Array.isArray(cartShopItems) && cartShopItems.length === 0){
+            if(cartShopItems[objFrom.count] === 1 ){
+                cartShopItems.push(objFrom)
+
+            }
+        }
+
+        cartShopItems.forEach(itemCart => {
+            const elementDivCreate = document.createElement('div')
+            elementDivCreate.setAttribute('class', 'cart_container')
+            elementDivCreate.innerHTML = `
+                <h5>${itemCart.title}</h5>
+                <p>${itemCart.price}</p>
+                <b>Cantidad: ${itemCart.count}</b>`
+
+            pintandoCart.appendChild(elementDivCreate)
+        })
+    }
+}
+
+const showPopUp = ({target:{dataset:{ctahero, closepopup}}}) => {
+    const pop_up = document.querySelector('.pop-up');
+    const ChildPop_up = document.querySelector('.pop-up > .pop__up');
+    if(ctahero === 'true'){
+        pop_up.classList.add('showPop__up')   
+        ChildPop_up.classList.add('showPop__up')   
+    }
+    if(closepopup === 'close'){
+        pop_up.classList.remove('showPop__up')
+        ChildPop_up.classList.remove('showPop__up')
+    }
+}
+
+
+
+
+
+
+window.addEventListener("DOMContentLoaded", async () => {
+    const resultData = await fetchDataProduct()
+
+    dataProductDestacado(resultData)
+
+    dataProductOfertas(resultData)
+})
+
+const dataProductOfertas = ({productosOferta}) => {
     let numberIdOfertas = 1
 
-    const filterDataOferta = datas.filter(data => data.productId === numberIdOfertas)
+    const filterDataOferta = productosOferta.filter(data => data.productId === numberIdOfertas)
 
     filterDataOferta.forEach(product => {
         template__card__oferta.querySelector('.card__img').setAttribute('src', product.img)
         template__card__oferta.querySelector('.card__title').textContent = product.title
         template__card__oferta.querySelector('.card__paragraph').textContent = product.description
-        template__card__oferta.querySelector('.btn--product').setAttribute('data-id', product.id)
+        template__card__oferta.querySelector('.btn--product').setAttribute('data-id', product.productId)
         template__card__oferta.querySelector('.card__price span').textContent = product.price
         template__card__oferta.querySelector('.card__price .oldPrice').textContent = product.oldPrice
 
@@ -44,16 +112,16 @@ const dataProductOfertas = (datas) => {
     wrapper_oferta.appendChild(fragment)
 }
 
-const dataProductDestacado = (datas) => {
+const dataProductDestacado = ({productosDestacados}) => {
     let numberIdDestacados = 1
 
-    const filterData = datas.filter(data => data.productId === numberIdDestacados)
+    const filterData = productosDestacados.filter(data => data.productId === numberIdDestacados)
 
     filterData.forEach(product => {
         template__card__destacados.querySelector('.card__img').setAttribute('src', product.img)
         template__card__destacados.querySelector('.card__title').textContent = product.title
         template__card__destacados.querySelector('.card__paragraph').textContent = product.description
-        template__card__destacados.querySelector('.btn--product').setAttribute('data-id', product.id)
+        template__card__destacados.querySelector('.btn--product').setAttribute('data-id', product.productId)
         template__card__destacados.querySelector('.card__price span').textContent = product.price
 
         const cloneDestacados = template__card__destacados.cloneNode(true)
